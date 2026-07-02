dedfe2f701785a3d6d00950d71dd6d059536e118
Harsh Krishnani <harsh.k@incubyte.co>
2026-07-02 11:58:40 +0530

    test: add failing test for empty string returns 0

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,11 @@ require 'spec_helper'
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
+  describe '.add' do
+    context 'when the input is an empty string' do
+      it 'returns 0' do
+        expect(described_class.add('')).to eq(0)
+      end
+    end
+  end
 end
