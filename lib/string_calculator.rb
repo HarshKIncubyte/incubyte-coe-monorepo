e639f327fa6c675422a75f4561e0e4e1e1a509f8
Harsh Krishnani <harsh.k@incubyte.co>
2026-07-02 13:15:50 +0530

    refactor: extract delimiter parsing into private class method with guard clause

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,13 +4,17 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    if numbers.start_with?('//')
-      delimiter = numbers[2]
-      numbers = numbers.split("\n", 2).last
-    else
-      delimiter = /,|\n/
-    end
-
+    delimiter, numbers = extract_delimiter(numbers)
     numbers.split(delimiter).sum(&:to_i)
   end
+
+  def self.extract_delimiter(numbers)
+    return [/,|\n/, numbers] unless numbers.start_with?('//')
+
+    delimiter = numbers[2]
+    numbers = numbers.split("\n", 2).last
+    [delimiter, numbers]
+  end
+
+  private_class_method :extract_delimiter
 end
