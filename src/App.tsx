67366660a01cfc70647f329dd70b74337b3b4e26
Harsh Krishnani <harsh.k@incubyte.co>
2026-07-08 15:57:45 +0530

    Initial React TypeScript application setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>GraphQL Learning Frontend</h1>
+      <p>React + TypeScript + Apollo Client</p>
+    </div>
+  )
+}
+
+export default App
