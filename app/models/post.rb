1ce4cfd77628281482770d3bc91e8b57e7a430b8
Harsh Krishnani <harsh.k@incubyte.co>
2026-07-14 10:44:53 +0530

    Include Elasticsearch::Model in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   belongs_to :user
   scope :published, -> { where(published: true) }
 end
