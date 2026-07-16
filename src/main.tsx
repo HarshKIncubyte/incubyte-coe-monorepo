68379f96d165664d73b3678e7cb13e7c23745b7d
Harsh Krishnani <harsh.k@incubyte.co>
2026-07-16 16:21:41 +0530

    Set up Chakra UI dark mode with next-themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ApolloProvider } from '@apollo/client/react'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
+import { ColorModeProvider } from './components/ui/color-mode'
 import './index.css'
 import App from './App'
 import client from './apollo/client'
@@ -9,9 +11,13 @@ import { UserProvider } from './context/UserContext'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ApolloProvider client={client}>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <ChakraProvider value={defaultSystem}>
+        <ColorModeProvider>
+          <UserProvider>
+            <App />
+          </UserProvider>
+        </ColorModeProvider>
+      </ChakraProvider>
     </ApolloProvider>
   </StrictMode>,
 )
